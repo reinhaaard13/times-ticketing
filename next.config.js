5651f4d74fe73086aa560e002d62913373e6af54
reinhaaard13 <reinhardkevin.rk@gmail.com>
2022-08-03 17:20:04 +0700

    add ngrok to images domains

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "rei-aws.s3.ap-southeast-1.amazonaws.com"],
+    domains: ["localhost", "rei-aws.s3.ap-southeast-1.amazonaws.com", "04ef-103-52-46-164.ap.ngrok.io"],
   }
 }
 
