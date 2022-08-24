0bc5a619d6627622f28a34204ce6acae53ba989a
reinhaaard13 <reinhardkevin.rk@gmail.com>
2022-08-24 16:52:14 +0700

    add dockerfile

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ["localhost", "rei-aws.s3.ap-southeast-1.amazonaws.com", "04ef-103-52-46-164.ap.ngrok.io"],
-  }
+  },
+  output: "standalone"
 }
 
 module.exports = nextConfig
