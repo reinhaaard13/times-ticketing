7ec614adf4fe3fda2018f1ce3e1d2b6f0197e44d
GithubDSP <github.digitalsolusipratama@gmail.com>
2022-08-31 15:42:41 +0700

    add next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/times',
   reactStrictMode: false,
   images: {
     domains: ["localhost", "rei-aws.s3.ap-southeast-1.amazonaws.com", "04ef-103-52-46-164.ap.ngrok.io"],
